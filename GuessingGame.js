60342180212bf76cbaa8fda998d63fe145bc8e0f
ccampbell <ccampbell@Administrators-MacBook-Pro-3.local>
2020-03-06 14:00:08 -0500

    Initial Portfolio Commit

---
+++ GuessingGame.js
@@ -0,0 +1,81 @@
+$(function () {
+    var num1 = Math.floor(Math.random() * 9) + 1;
+    var num2 = Math.floor(Math.random() * 9) + 1;
+    var num3 = Math.floor(Math.random() * 9) + 1;
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+
+    while (num1 == num2 || num1 == num3 || num2 == num3) {
+        num1 = Math.floor(Math.random() * 9) + 1;
+        num2 = Math.floor(Math.random() * 9) + 1;
+        num3 = Math.floor(Math.random() * 9) + 1;
+
+        console.log(num1);
+        console.log(num2);
+        console.log(num3);
+    }
+
+    $("#subButton").on("click", function () {
+        if ($("#in1").val() == num1) {
+            $("#column1").css("background-color", "rgb(93, 148, 81)");
+        }
+        else if ($("#in1").val() == num2 || $("#in1").val() == num3) {
+            $("#column1").css("background-color", "darkgoldenrod");
+        }
+        else $("#column1").css("background-color", "firebrick");
+
+        if ($("#in2").val() == num2) {
+            $("#column2").css("background-color", "rgb(93, 148, 81)");
+        }
+        else if ($("#in2").val() == num1 || $("#in2").val() == num3) {
+            $("#column2").css("background-color", "darkgoldenrod");
+        }
+        else $("#column2").css("background-color", "firebrick");
+
+        if ($("#in3").val() == num3) {
+            $("#column3").css("background-color", "rgb(93, 148, 81)");
+        }
+        else if ($("#in3").val() == num1 || $("#in3").val() == num2) {
+            $("#column3").css("background-color", "darkgoldenrod");
+        }
+        else $("#column3").css("background-color", "firebrick");
+
+        if (($("#in1").val() == num1) && ($("#in2").val() == num2) && ($("#in3").val() == num3)) {
+	          $("#column3").css("background-color", "rgb(93, 148, 81)");
+            alert("You Win!!!!!!");
+            $("#reload").fadeIn();
+            $("#subButton").fadeOut();
+        }
+    });
+
+    var currentLives = 10;
+
+    $("#subButton").on("click", function () {
+        currentLives = currentLives - 1;
+        $("#lives").html("You have " + currentLives + " lives left!");
+        if (currentLives == 0) {
+            alert("You Lose");
+
+            $("#subButton").fadeOut();
+            $("#reload").fadeIn();
+        }
+
+    });
+    $("#reload").hide();
+    $("#reload").on("click", function () {
+        location.reload();
+    });
+
+});
+
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
---
+++ TipCalculator.js
@@ -0,0 +1,50 @@
+$(function () {
+    $("#calculate").on("click", calculateTipVariables);
+    $("#tipResult").hide();
+    $("#resetButton").hide();
+    $("#resetButton").on("click", function () {
+        location.reload();
+    });
+});
+
+function calculateTipVariables() {
+    var billAmount = $("#billText").val();
+    var service = $("#serviceSelect").val();
+    var split = $("#billSplit").val();
+
+    if (billAmount == "" || service == null) {
+        alert("Input All Fields!")
+    }
+    else {
+        console.log(billAmount);
+        console.log(service);
+        console.log(split);
+        calculateTip(billAmount, service, split);
+    }
+}
+
+function calculateTip(billAmount, service, split) {
+    var result = (+billAmount * +service / +split).toFixed(2);
+    console.log(result);
+    $("#totalTip").append("$" + result);
+    if (split > 1) {
+        var totalResult = ((+billAmount / +split) + +result).toFixed(2);
+        $("#totalBillAmount").append("$" + totalResult);
+    }
+    else {
+        var totalSoloResult = (+billAmount + +result).toFixed(2);
+        $("#totalBillAmount").append("$" + totalSoloResult);
+    }
+    $("#tipResult").fadeIn();
+    $("#resetButton").fadeIn();
+    $("#calculate").fadeOut();
+}
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    } else {
+        return true;
+    }
+}
